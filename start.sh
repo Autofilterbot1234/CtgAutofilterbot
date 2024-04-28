25ddb141a0234eed3a0448ce214494caf29116da
Autofilterbot1234 <168348712+Autofilterbot1234@users.noreply.github.com>
2024-04-28 19:34:50 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/Autofilterbot1234/CtgAutofilterbot/commits?author=Autofilterbot1234 /CtgAutofilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /CtgAutofilterbot
 fi
-cd /AutoFilterAdvance
+cd /CtgAutofilterbot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
